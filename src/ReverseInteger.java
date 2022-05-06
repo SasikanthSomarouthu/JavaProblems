3c52fb4d769db9c39e783d42760b2c853b7b7a6e
Sasikanth Somarouthu <somarouthusasikanth@gmail.com>
2022-05-06 00:39:52 -0400

    Just changing something

--- src/ReverseInteger.java
+++ src/ReverseInteger.java
@@ -2,26 +2,23 @@ import java.util.Scanner;
 
 public class ReverseInteger {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        
-        sc.close();
-        
-        try 
-        {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
 
-         StringBuilder sb=new StringBuilder(String.valueOf(a)).reverse().insert(0,"-");
-      
-        int reverse=Integer.parseInt(sb.toString());
-        System.out.println(reverse);
+        sc.close();
+
+        try {
+
+            StringBuilder sb = new StringBuilder(String.valueOf(a)).reverse().insert(0, "+");
+
+            int reverse = Integer.parseInt(sb.toString());
+            System.out.println("This is the reverse of the number", a, reverse);
+        } catch (Exception e) {
+            System.out.println(e);
         }
-        catch(Exception e){
-        	System.out.println(e);
-        }
-        
-        
-	}
+
+    }
 
 }
