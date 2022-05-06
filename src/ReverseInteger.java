6f9c2017c38c5d83c65b437b5b965e8ed2eddad5
Sasikanth Somarouthu <somarouthusasikanth@gmail.com>
2022-05-06 00:44:18 -0400

    Updating print statment

--- src/ReverseInteger.java
+++ src/ReverseInteger.java
@@ -14,7 +14,7 @@ public class ReverseInteger {
             StringBuilder sb = new StringBuilder(String.valueOf(a)).reverse().insert(0, "+");
 
             int reverse = Integer.parseInt(sb.toString());
-            System.out.println("This is the reverse of the number", a, reverse);
+            System.out.println("This is the reverse of the number" + a + "is" + reverse);
         } catch (Exception e) {
             System.out.println(e);
         }
